34b4c07bb6cd95b21c6e35c0534595d0c161b0ef
NekoZ-Meow <okc5424938d474w@gmail.com>
2021-10-31 20:46:45 +0900

    開発環境を整えた

---
+++ CSV2HTML_by_Java/csv2html/SampleTest.java
@@ -0,0 +1,23 @@
+package csv2html;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 仮のテスト
+ * 動くか試すだけ
+ */
+class SampleTest {
+    /**
+     * とりあえずサンプルで実行してみるテスト
+     */
+    @Test
+    public void testSuccess() {
+        assertTrue(true);
+    }
+
+    // @Test
+    // public void testFailed() {
+    //     assertEquals(true, false);
+    // }
+}
