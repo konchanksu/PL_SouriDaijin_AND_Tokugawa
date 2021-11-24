0a9e127df89df74ae858bc7b9cf7b29c42088942
Kunoshita <shuuimonogatari@gmail.com>
2021-11-24 16:04:57 +0900

    Tabの修正と名称の変更

--- CSV2HTML_by_Java/csv2html/TranslatorTest.java
+++ CSV2HTML_by_Java/csv2html/TranslatorTest.java
@@ -8,45 +8,48 @@ import org.junit.jupiter.api.Test;
  */
 public class TranslatorTest{
 
+	/**
+	 * Attributesの具象クラスを用いて、Translatorのインスタンスを作成する
+	 */
 	private Translator aTranslator = new Translator(ExtendsAttributesUsedTest.class);
 
 	/**
 	 * 1日が正しく計算できているかのテスト
 	 */
 	@Test
-	public void caluculateOneDay() {
+	public void caluculateOneDayTest() {
         assertEquals("2", this.aTranslator.computeNumberOfDays("2020年1月1日〜2020年1月2日"));
     }
 
     /**
 	 * 1週間が正しく計算できているかのテスト
 	 */
-    @Test
-	public void caluculateOneWeek() {
+	@Test
+	public void caluculateOneWeekTest() {
         assertEquals("8", this.aTranslator.computeNumberOfDays("2020年1月1日〜2020年1月8日"));
     }
 
     /**
 	 * 1年が正しく計算できているかのテスト
 	 */
-    @Test
-	public void caluculateOneYear() {
+	@Test
+	public void caluculateOneYearTest() {
 		assertEquals("366", this.aTranslator.computeNumberOfDays("2019年1月1日〜2020年1月1日"));
     }
 
 	/**
 	 * 1年(うるう年)が正しく計算できているかのテスト
 	 */
-    @Test
-	public void caluculateLeapYear() {
+	@Test
+	public void caluculateLeapYearTest() {
 		assertEquals("367", this.aTranslator.computeNumberOfDays("2020年1月1日〜2021年1月1日"));
     }
 	
     /**
 	 * 1752年の9月2日と14日の間が正しく計算できているかのテスト
 	 */
-    @Test
-	public void caluculateGregorian() {
+	@Test
+	public void caluculateGregorianTest() {
 		assertEquals("2", this.aTranslator.computeNumberOfDays("1752年9月2日〜1752年9月14日"));
     }
 }
