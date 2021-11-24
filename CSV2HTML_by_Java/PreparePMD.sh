8ca3a5e74fc82819de74cd922bfc42c468fa6a21
NekoZ-Meow <okc5424938d474w@gmail.com>
2021-11-24 17:17:38 +0900

    PreparePMD修正

--- CSV2HTML_by_Java/PreparePMD.sh
+++ CSV2HTML_by_Java/PreparePMD.sh
@@ -3,6 +3,9 @@
 : <<EOF_COMMENT
     PMDをダウンロード・展開し、PMDのbinディレクトリのシンボリックリンクを作成する.
     また、pmd-rulesが存在しない場合はそれもダウンロードする
+
+    author : Okayama Kodai
+    version : 1.0.1
 EOF_COMMENT
 
 #これらはMakefileと文字列を合わせる
@@ -13,26 +16,22 @@ PMD_RULES="pmd-rules.xml"
 PMD_VERSION="6.40.0"
 PMD_DIR="pmd-bin-${PMD_VERSION}"
 PMD_URL="https://github.com/pmd/pmd/releases/download/pmd_releases%2F${PMD_VERSION}/${PMD_DIR}.zip"
-#下のURLは将来的にこのプロジェクトのリポジトリのxmlを参照するかも
 PMD_RULES_URL="https://raw.githubusercontent.com/pmd/pmd/master/pmd-java/src/main/resources/rulesets/java/quickstart.xml"
 
-if [ -e ${PMD_BASE_DIR} ];then
-    if [ ! -f "${PMD_BASE_DIR}/${PMD_RULES}" ];then
-        curl -L ${PMD_RULES_URL} -o "./${PMD_BASE_DIR}/${PMD_RULES}"
-    fi
-
-    if [ -d "${PMD_BASE_DIR}/${PMD_DIR}" ];then
-        ln -s "${PMD_BASE_DIR}/${PMD_DIR}/bin" ${PMD_BIN}
-        exit 0
-    fi
-    rm -rf ${PMD_BASE_DIR}
+if [ ! -e ${PMD_BASE_DIR} ]; then
+    mkdir ${PMD_BASE_DIR}
 fi
 
-mkdir ${PMD_BASE_DIR}
-curl -L ${PMD_RULES_URL} -o "./${PMD_BASE_DIR}/${PMD_RULES}"
-curl -OL ${PMD_URL}
-unzip ${PMD_DIR}.zip -d "${PMD_BASE_DIR}/"
-rm ${PMD_DIR}.zip
+if [ ! -f "${PMD_BASE_DIR}/${PMD_RULES}" ]; then
+    curl -L ${PMD_RULES_URL} -o "./${PMD_BASE_DIR}/${PMD_RULES}"
+fi
+
+if [ ! -d "${PMD_BASE_DIR}/${PMD_DIR}" ]; then
+    curl -OL ${PMD_URL}
+    unzip ${PMD_DIR}.zip -d "${PMD_BASE_DIR}/"
+    rm ${PMD_DIR}.zip
+fi
+
 ln -s "${PMD_BASE_DIR}/${PMD_DIR}/bin" ${PMD_BIN}
 
 exit 0
