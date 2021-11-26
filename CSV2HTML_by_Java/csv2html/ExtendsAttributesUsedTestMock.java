d83ca335e6c9ab2cca0d1b734b36a26d6c4df5ea
Kunoshita <shuuimonogatari@gmail.com>
2021-11-26 19:16:36 +0900

    クラス名にMockを追加

---
+++ CSV2HTML_by_Java/csv2html/ExtendsAttributesUsedTestMock.java
@@ -0,0 +1,23 @@
+package csv2html;
+
+/**
+ * Attributesの具象クラス
+ */
+public class ExtendsAttributesUsedTest extends Attributes{
+    @Override
+    String captionString(){
+        return "";
+    }
+    String baseDirectory(){
+        return "";
+    }
+    String baseUrl(){
+        return "";
+    }
+    String csvUrl(){
+        return "";
+    }
+    String titleString(){
+        return "";
+    }
+}
