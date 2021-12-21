5b32479bc56724700baef3c6d7cf1df06b2f9c16
Kunoshita <shuuimonogatari@gmail.com>
2021-12-21 19:47:03 +0900

    内容の修正とリント掛け

--- CSV2HTML_by_Python/csv2html/downloader.py
+++ CSV2HTML_by_Python/csv2html/downloader.py
@@ -9,6 +9,7 @@ __date__ = '2021/01/10 (Created: 2016/01/01)'
 import os
 # import shutil
 import urllib.request
+import threading
 
 from csv2html.io import IO
 from csv2html.reader import Reader
@@ -49,23 +50,35 @@ class Downloader(IO):
 
 		base_url = super().attributes().base_url()
 		base_dir = super().attributes().base_directory()
+		thread_list = []
 
 		# 画像ファイルをダウンロード
 		for image_filename in image_filenames:
-			with urllib.request.urlopen(base_url + image_filename) as a_file:
-				image_data = a_file.read()
+			thread = threading.Thread(target=self.download_image, args=(base_url, base_dir, image_filename,))
+			thread_list.append(thread)
+			thread.start()
+		for thread in thread_list:
+			thread.join()
 
-			# ファイルが存在しない場合は生成
-			a_directory = os.path.dirname(base_dir + os.sep + image_filename)
-			os.makedirs(a_directory, exist_ok=True)
+	def download_image(self, base_url, base_dir, image_filename):
+		with urllib.request.urlopen(base_url + image_filename) as a_file:
+			image_data = a_file.read()
 
-			# 書き出し
-			with open(base_dir + os.sep + image_filename, mode="wb") as a_image_file:
-				a_image_file.write(image_data)
+		# ファイルが存在しない場合は生成
+		a_directory = os.path.dirname(base_dir + os.sep + image_filename)
+		os.makedirs(a_directory, exist_ok=True)
+
+		# 書き出し
+		with open(base_dir + os.sep + image_filename, mode="wb") as a_image_file:
+			a_image_file.write(image_data)
 
 	def perform(self):
 		"""すべて（情報を記したCSVファイル・画像ファイル群・縮小画像ファイル群）をダウンロードする。"""
 		self.download_csv()
 		Reader(super().table()).perform()
-		self.download_images(super().table().thumbnail_filenames())
-		self.download_images(super().table().image_filenames())
+		thumbnailThread = threading.Thread(target=self.download_images, args=(super().table().thumbnail_filenames(),))
+		imageThread = threading.Thread(target=self.download_images, args=(super().table().image_filenames(),))
+		thumbnailThread.start()
+		imageThread.start()
+		thumbnailThread.join()
+		imageThread.join()
